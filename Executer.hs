3cec8e2c78cd5b0bc4aca24722998cc0e0d2fed5
Carlos Lopez-Camey <c.lopez@kmels.net>
2014-03-11 20:29:21 -0600

    There exists now a console that reads a line (intended command)

--- Executer.hs
+++ Executer.hs
@@ -8,16 +8,18 @@ import Database
 
 -- | 
 -- Executive funs
+data Result = OK | Error String deriving Show
 
-execute :: Command -> IO ()
+execute :: Command -> IO Result
 execute (CREATEDB dbname) = createDB . unpack $ dbname
 
 -- | Creates a database file
-createDB :: String -> IO ()
+createDB :: String -> IO Result
 createDB db_name = do
 	 -- make an empty database
 	 let new_db = Database { name = db_name, num_reg = 0 }
 	 -- update the list of databases 
 	 dbs <- getDatabases
          writeDatabases (new_db:dbs)
+         return $ OK
 	 
--- Main.hs
+++ Main.hs
@@ -4,13 +4,48 @@ import Commands
 import Data.Text
 import Executer
 import Parser
+import System.IO
+import System.IO.Unsafe
+main = do
+  --listen to commands
+  hFlush stdout
+  input <- readCommand
+  interpret input
+  
+  where
+    interpret :: String -> IO ()
+    interpret input = do
+      let command' = parseOnly commandParser (pack input)
+      case command' of
+        (Right command) -> do
+          putStrLn $ "Executing ..."
+          result <- execute command
+          putStrLn $ show result          
+        (Left err) -> do
+          putStrLn $ err
+          putStrLn $ "Could not understand your input"
+      -- do it again, for the sake of IO
+      readCommand >>= interpret
 
-main = do     
-     let input = pack $ "CREATE DATABASE nombreDeMiDB"
-     
-     let cmd = parseOnly commandParser input
+    readCommand :: IO String
+    readCommand = do
+      hFlush stdout
+      putStr "azhen> " >> hFlush stdout
+      input <- getLine
+      return input
+      
+{-
+   interact consoleInterpreter
+  where
+    consoleInterpreter :: String -> String
+    consoleInterpreter input = do      
+      --let input = pack $ "CREATE DATABASE nombreDeMiDB"     
+      let _ = unsafePerformIO $ putStrLn $ "Read " ++ input
+      let cmd = parseOnly commandParser (pack input)
 
-     case cmd of
-       (Right command) -> execute command
-       (Left s) -> error $ "Parsing " ++ s
+      case cmd of
+        (Right command) -> let result = unsafePerformIO $ execute command
+                           in show result             
+        (Left s) -> error $ "Parsing " ++ s
      
+-}
