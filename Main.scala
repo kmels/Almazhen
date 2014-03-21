baa664de1d3af4138c740af3dc0c321dab060cc9
Carlos Lopez-Camey <c.lopez@kmels.net>
2014-03-20 22:31:37 -0600

    Hello World Scala

---
+++ Main.scala
@@ -0,0 +1,8 @@
+/**
+ * Created by kmels on 3/20/14.
+ */
+
+
+object Main  extends App{
+  override def main(args: Array[String]) = println("Hello, world!")
+}
---
+++ build.sbt
@@ -0,0 +1,4 @@
+name := "Almazhen"
+
+version := "1.0"
+    
