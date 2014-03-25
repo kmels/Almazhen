f41ca4f1fc92588fbf71a91c3f1c15faafd90973
Paulo <pigenio@gmail.com>
2014-03-25 00:11:22 -0600

    Parser Initialization
    Initialize Parser Implementation

--- Command.scala
+++ Command.scala
@@ -1,7 +1,24 @@
 
 class Command
 
-case class CreateDatabase(databaseName : String) extends Command
+case class CreateDatabase(databaseName : String) extends Command {
+  def execute = {
+
+  }
+}
+
+case class AlterDatabase(name : String,newName : String) extends Command {}
 
 
+case class DropDatabase(databaseName : String) extends Command {
+  def execute = {
 
+  }
+}
+case class ShowDatabases() extends Command {}
+
+case class UseDatabase(databaseName : String) extends Command {
+  def execute = {
+
+  }
+}
--- Parser.scala
+++ Parser.scala
@@ -1,7 +1,9 @@
 import scala.util.parsing.combinator.syntactical.StandardTokenParsers
 
 object Parser extends StandardTokenParsers {
-  lexical.reserved ++= Set("CREATE","DATABASE")
+  lexical.reserved ++= Set("CREATE","DATABASE","ALTER","DROP","SHOW","DATABASES","USE","TABLE","PRIMARY","KEY","FOREIGN"
+    ,"CHECK","INT","FLOAT","DATE","CHAR","AND","OR","NOT","RENAME","TO","ADD","COLUMN","CONSTRAINT","TABLES","COLUMNS",
+    "FROM","INSERT","INTO","VALUES","UPDATE","SET","WHERE","DELETE","SELECT","ORDER","BY","ASC","DESC","NULL")
   //lexical.delimiters ++= Set("=","/",",")
 
   def command:Parser[Command] = createDBCommandParser
@@ -9,6 +11,26 @@ object Parser extends StandardTokenParsers {
   def createDBCommandParser:Parser[CreateDatabase] = "CREATE" ~> "DATABASE" ~> ident ^^ {
     case name => CreateDatabase(name)
   }
+
+  def alterDBCommandParser:Parser[AlterDatabase] = "ALTER" ~> "DATABASE" ~> ident ~ "RENAME" ~ "TO" ~ ident ^^ {
+    case name ~ "RENAME" ~ "TO" ~ newName => AlterDatabase(name,newName)
+  }
+
+  def dropDBCommandParser:Parser[DropDatabase] = "DROP" ~> "DATABASE" ~> ident ^^ {
+    case name => DropDatabase(name)
+  }
+
+  def showDBCommandParser:Parser[ShowDatabases] = "SHOW" ~> "DATABASES" ^^ {
+    case _ => ShowDatabases()
+  }
+
+  def useDBCommandParser:Parser[UseDatabase] = "USE" ~> "DATABASE" ~> ident ^^ {
+    case name => UseDatabase(name)
+  }
+
+//  def createTBCommandParser:Parser[DropDatabase] = "CREATE" ~> "TABLE" ~> ident ~ "(" ~ repsep(ColumnSpec,",") ~ "CONSTRAINT" ~ repsep(Restriction, ",") ~ ")" ^^ {
+//    case name ~ "(" ~ List[ColumnSpec] ~ "CONSTRAINT" ~ => CreateTable(name)
+//  }
 
   def parse(s:String):Option[Command] = {
     val tokens = new lexical.Scanner(s)
