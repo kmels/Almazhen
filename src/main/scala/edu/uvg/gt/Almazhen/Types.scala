4d9e57a7d7d4f7df9b53583ed7f4f2bf42dc8ab8
Paulo Chang <pigenio@gmail.com>
2014-05-01 16:34:05 -0600

    Implicit Table definition

--- src/main/scala/edu/uvg/gt/Almazhen/Types.scala
+++ src/main/scala/edu/uvg/gt/Almazhen/Types.scala
@@ -6,14 +6,63 @@ package edu.uvg.gt.Almazhen
 
 import java.io.{InputStreamReader, BufferedReader}
 import java.util.Scanner;
+import scalaz._, Scalaz._
+import argonaut._, Argonaut._
 
 case class Database(name: String, table_count: Int)
 
 case class Table(name: String, cols: List[String/*ColumnDefinition*/], restrictions: List[String/*Constraint*/])
 
-case class ColumnDefinition(name: String, typ: AZtype)
+case class ColumnDefinition(name: String, typ: AZtype) 
 
-case class Predicate
+object Implicits { 
+  implicit def ColumnDefinitionsJson : CodecJson[ColumnDefinition] = casecodec2(ColumnDefinition.apply, ColumnDefinition.unapply)("name","type")
+  implicit def AZtypeEncodeJson: EncodeJson[AZtype] =
+    EncodeJson((a: AZtype) =>
+      ("theType" := (a match {
+        case IntType => "Int"
+        case FloatType => "Float"
+        case DateType => "Date" 
+        case CharType => "Char" 
+      }) ) ->: jEmptyObject)
+      
+  implicit def AZtypeDecodeJson: DecodeJson[AZtype] =
+    DecodeJson(c => for {
+			      theType <- (c --\ "theType").as[String]
+			    }  
+		    	yield (theType match{
+		    	  case "Int" => IntType
+		    	  case "Float" => FloatType
+		    	  case "Date" => DateType
+		    	  case "Char" => CharType
+		    	})
+    		)
+    
+    implicit def ConstraintEncodeJson : EncodeJson[Constraint] = EncodeJson(encodeConstraint)
+    implicit def ConstraintDecodeJson : DecodeJson[Constraint] = DecodeJson(decodeConstraint)
+    
+    def encodeConstraint(a: Constraint): argonaut.Json = a match{
+    	case Pk_key(name, cols) => 
+    	  (("theType" := "pk_key") ->: ("name" := name) ->: ("cols" := cols) ->: jEmptyObject)
+    	case Fk_key(name, cols) => 
+    	  (("theType" := "fk_key") ->: ("name" := name) ->: ("cols" := cols) ->: jEmptyObject)
+    	case Ch_key(name, check) => 
+    	  (("theType" := "ch_key") ->: ("name" := name) ->: ("predicate" := "theCheck") ->: jEmptyObject)  
+    }
+  
+	def decodeConstraint(c: argonaut.HCursor): DecodeResult[Pk_key] = 
+	  
+	  //return Pk_key((c --\ "name").as[String], (c --\ "cols").as[List[String]])
+	  
+	  for {
+      theType <- (c --\ "theType").as[String]
+      name <- (c --\ "name").as[String]
+      cols <- (c --\ "cols").as[List[String]]
+    } yield ( Pk_key(name, cols) )
+	
+}
+
+case class Predicate(expression: String)
 
 case class OrderBy(expression : String, order : String)
 
