6397885fcf46c86a44b8f83759360d475b98f737
Carlos Lopez-Camey <c.lopez@kmels.net>
2014-05-23 22:49:00 -0600

    Fetches and inserts column values

--- almazhen-crud/src/main/scala/uvg/Almazhen/MainServlet.scala
+++ almazhen-crud/src/main/scala/uvg/Almazhen/MainServlet.scala
@@ -2,17 +2,38 @@ package uvg.Almazhen
 
 import org.scalatra._
 import scalate.ScalateSupport
+import edu.uvg.gt.Almazhen.{ColumnDefinition, AZtype, IntType, FloatType, DateType, VARCHAR}
+import java.text.SimpleDateFormat
+import java.util.NoSuchElementException
 
 class MainServlet extends AlmazhenCrudStack {
   final val TABLE_NAME = ""
 
   get("/") {
+    val table_name = DBTable.table_name
+    val table_columns = DBTable.getTableColumns
+    val table_values:List[List[String]] = DBTable.values
+
     <html>
       <body>
-        <h1>Hello, world!</h1>
-        Say <a href="hello-scalate">hello to Scalate</a>.
+        <h1>Tabla: {table_name}</h1>
+        <h1>Columnas</h1>
+        <ul>
+          { table_columns.map(p => <li> {p.name }: {p.typ} </li> ) }
+        </ul>
+        <h1>Valores</h1>
+        <ul>
+          { table_values.map(row => <li> {row.mkString("\t")}</li> ) }
+        </ul>
+        <a href="/recrear">Recrear tabla</a>
       </body>
     </html>
+  }
+
+  get("/recrear"){
+    println("Recreando")
+    DBTable.remake
+    redirect("/")
   }
 
   post("/eliminar/:id"){
@@ -22,12 +43,48 @@ class MainServlet extends AlmazhenCrudStack {
 
   }
 
+  def defaultValue(typ: AZtype): String = typ match{
+    case IntType => 0.toString
+    case FloatType => 0.0f.toString
+    case VARCHAR(_) => ""
+    case DateType => {
+      val df = new SimpleDateFormat("yyyy-MM-dd")
+      df.format(new java.util.Date())
+    }
+  }
+
   post("/insertar/fila"){
     //val columns
 
-    //params("id")
+    println(params.toString())
+    
+    val columns: List[ColumnDefinition] = DBTable.getTableColumns
 
-    println("Las columnas son "+DBTable.getTableColumns)
+    val values: List[String] = columns.map{
+      case ColumnDefinition(name, typ) => try {
+          params(name) match{
+            case "" => defaultValue(typ)
+            case value => value
+          }
+        } catch{ case e: NoSuchElementException => {
+          println("No se puede encontrar el parametro enviado para "+name)
+          defaultValue(typ)
+        }}
+    }
+
+
+    println("Insertando valores: "+values)
+
+    DBTable.insert(values) match{
+      case Left(err) => <html>
+        <body>
+          <h1>Error: {err.toString}</h1>
+        </body>
+      </html>
+      case Right(_) => "OK"
+    }
+
+    println("Las columnas son "+columns)
   }
-  
+
 }
--- almazhen-crud/src/main/scala/uvg/Almazhen/Table.scala
+++ almazhen-crud/src/main/scala/uvg/Almazhen/Table.scala
@@ -1,10 +1,10 @@
 package uvg.Almazhen
 
 import edu.uvg.gt.Almazhen.ColumnDefinition
-import edu.uvg.gt.Almazhen.Databases
-import edu.uvg.gt.Almazhen.Constraint
+import edu.uvg.gt.Almazhen.{Databases, Database}
+import edu.uvg.gt.Almazhen.{Constraint, Rows, ShowRows, Row}
 import edu.uvg.gt.Almazhen._
-import edu.uvg.gt.Almazhen.{Table, Tables, IntType, Pk_key}
+import edu.uvg.gt.Almazhen.{Table, Tables, DateType, IntType, FloatType, Pk_key}
 
 /**
  * Created by kmels on 23/05/14.
@@ -13,8 +13,17 @@ object DBTable {
   Databases.create("CRUD")
   Databases.use("CRUD")
 
+  def db: Database = Databases.use("CRUD") match{
+    case None => Databases.create("CRUD").fold[Database](null)(db => db)
+    case Some(db) => db
+  }
+
   final val initialColumns: List[ColumnDefinition] = List(
-    ColumnDefinition("id", IntType)
+    ColumnDefinition("id", IntType),
+    ColumnDefinition("nombre", VARCHAR(20)),
+    ColumnDefinition("apellido", VARCHAR(20)),
+    ColumnDefinition("credito", FloatType),
+    ColumnDefinition("nacimiento", DateType)
   )
 
   final val columnConstraints: List[Constraint] = List(
@@ -31,6 +40,24 @@ object DBTable {
   private def table: Table = Tables.findTableByName(table_name) match{
     case Some(t) => t
     case _ => makeTable
+  }
+
+  def remake: Table = {
+    println("Borrando tabla ...")
+    Tables.drop(db, table_name)
+    println("Creando tabla ...")
+    this.makeTable
+  }
+
+  def insert(values: List[String]) = Rows.insertInto(db, table, None, values)
+
+  def values:List[List[String]] = Rows.selectFrom(db, table, None, None, Nil) match{
+    case Right(ShowRows(colnames, rows)) => rows.map(row => row.values.map(_.column_value))
+    case Left(err) => {
+      println("Error: "+err.toString())
+      Nil
+    }
+    case _ => Nil //exhaustive match
   }
 
   def getTableColumns: List[ColumnDefinition] = table.columns
